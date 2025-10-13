3e94d2c2740ff2bba56031b9b41b603e7c1b58a6
yahya <yahiamohammad740@gmail.com>
2025-10-13 13:16:04 +0300

    Bottom Navigation bar, Cards, and ListTiles

--- personal_profile_ui/lib/main.dart
+++ personal_profile_ui/lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,6 +14,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Personal Profile UI Demo',
       debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
+        textTheme: GoogleFonts.poppinsTextTheme(Theme.of(context).textTheme),
+      ),
       home: MyHomePage(),
     );
   }
@@ -55,7 +61,6 @@ class MyHomePage extends StatelessWidget {
               ),
             ],
           ),
-
           SizedBox(height: 25),
           Row(
             children: [
@@ -87,9 +92,123 @@ class MyHomePage extends StatelessWidget {
               );
             }),
           ),
-          SizedBox(height: 10),
+          SizedBox(height: 25),
+          SizedBox(
+            height: 180,
+            child: ListView.separated(
+              physics: BouncingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (context, index) {
+                final card = profileCompletetionCards[index];
+                return SizedBox(
+                  width: 160,
+                  child: Card(
+                    elevation: 4,
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(card.icon, size: 30),
+                          Text(card.title, textAlign: TextAlign.center),
+                          ElevatedButton(
+                            onPressed: () {},
+                            child: Text(card.buttonText),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+              separatorBuilder: (context, index) =>
+                  Padding(padding: EdgeInsets.only(right: 5)),
+              itemCount: profileCompletetionCards.length,
+            ),
+          ),
+          SizedBox(height: 25),
+          ...List.generate(customListTiles.length, (index) {
+            final tile = customListTiles[index];
+            return Padding(
+              padding: const EdgeInsets.only(bottom: 5),
+              child: Card(
+                elevation: 4,
+                child: ListTile(
+                  leading: Icon(tile.icon),
+                  title: Text(tile.title),
+                  trailing: Icon(Icons.chevron_right),
+                ),
+              ),
+            );
+          }),
+        ],
+      ),
+
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 3,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.home),
+            label: "Home",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.chat_bubble_2),
+            label: "Messages",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.book),
+            label: "Discover",
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(CupertinoIcons.person),
+            label: "Profile",
+          ),
         ],
       ),
     );
   }
 }
+
+class ProfileCompletionCard {
+  final IconData icon;
+  final String title;
+  final String buttonText;
+  ProfileCompletionCard({
+    required this.icon,
+    required this.title,
+    required this.buttonText,
+  });
+}
+
+List<ProfileCompletionCard> profileCompletetionCards = [
+  ProfileCompletionCard(
+    title: "Set Your Profile Details",
+    icon: CupertinoIcons.person_circle,
+    buttonText: "Continue",
+  ),
+  ProfileCompletionCard(
+    title: "Upload your resume",
+    icon: CupertinoIcons.doc,
+    buttonText: "Upload",
+  ),
+  ProfileCompletionCard(
+    title: "Add your skills",
+    icon: CupertinoIcons.square_list,
+    buttonText: "Add",
+  ),
+];
+
+class CustomListTile {
+  final IconData icon;
+  final String title;
+
+  CustomListTile({required this.icon, required this.title});
+}
+
+List<CustomListTile> customListTiles = [
+  CustomListTile(icon: Icons.insights, title: "Activity"),
+  CustomListTile(icon: Icons.location_on_outlined, title: "Location"),
+  CustomListTile(title: "Notifications", icon: CupertinoIcons.bell),
+  CustomListTile(title: "Logout", icon: CupertinoIcons.arrow_right_arrow_left),
+];
